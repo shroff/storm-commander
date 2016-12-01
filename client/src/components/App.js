ce0143f1b54a2ab0b5d2de2586820a6c7e029248
Abhishek Shroff <ashroff6@gmail.com>
2016-12-01 01:26:03 -0800

    Fetch and show list of devices

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,35 @@
+import 'whatwg-fetch'
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      data: null
+    };
+  }
+  
+  componentDidMount() {
+    fetch('api/device_list')
+      .then((response) => {
+        return response.json()
+      }).then((jsonText) => {
+        this.setState({
+          loading: false,
+          data: jsonText
+        })
+      })
+  }
+
   render() {
-    return <h1>Hello App!</h1>
+    let data = this.state.loading ? "Loading..." : JSON.stringify(this.state.data)
+    return (
+      <div className="App">
+        <h1>Hello App!</h1>
+        <pre>{data}</pre>
+      </div>
+    )
   }
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router'
 import App from './components/App.js';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path="/" component={App} />
+  </Router>,
   document.getElementById('root')
 );
